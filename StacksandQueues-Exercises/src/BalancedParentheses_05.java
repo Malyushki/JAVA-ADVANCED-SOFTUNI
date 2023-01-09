d1c07715d74d49cc80bde3b79e3c5eb01420dcc8
malyushki <malyushki@gmail.com>
2023-01-09 13:55:58 +0200

    commit

--- StacksandQueues-Exercises/src/BalancedParentheses_05.java
+++ StacksandQueues-Exercises/src/BalancedParentheses_05.java
@@ -5,42 +5,47 @@ public class BalancedParentheses_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String parentheses = scanner.nextLine();
-        if (parentheses.length()%2!=0){
-            System.out.println("No");
-        }else {
-            int n = parentheses.length()/2;
+        if (parentheses.length() % 2 != 0 || parentheses.length()==0) {
+            System.out.println("NO");
+        } else {
             ArrayDeque<Character> stack = new ArrayDeque<>();
             boolean isBalanced = true;
-            for (int i = 0; i <n ; i++) {
-                stack.push(parentheses.charAt(i));
-            }
-            String parenthesesSubstring = parentheses.substring(n,parentheses.length());
-            char symbol = 0 ;
-            for (int i = 0; i <parenthesesSubstring.length() ; i++) {
-                switch (parenthesesSubstring.charAt(i)){
-                    case ')':
-                        symbol = '(';
+
+            for (int i = 0; i < parentheses.length(); i++) {
+               if (isBalanced){
+                switch (parentheses.charAt(i)) {
+                    case '(':
+                    case '[':
+                    case '{':
+                        stack.push(parentheses.charAt(i));
                         break;
-                    case '}':
-                        symbol = '{';
+                    case ')':
+                        if (stack.pop() != '(') {
+                            isBalanced = false;
+                        }
                         break;
                     case ']':
-                        symbol = '[';
+                        if (stack.pop() != '[') {
+                            isBalanced = false;
+                        }
+                        break;
+                    case '}':
+                        if (stack.pop() != '{') {
+                            isBalanced = false;
+                        }
                         break;
                 }
-                if (symbol!=stack.pop()){
-                    isBalanced = false;
-                    break;
-                }
+               }
             }
-            if (isBalanced){
-                System.out.println("YES");
-            }else {
-                System.out.println("NO");
-            }
+        if (isBalanced){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
 
         }
 
 
     }
+
 }
--- StacksandQueues-Exercises/src/SimpleTextEditor_07.java
+++ StacksandQueues-Exercises/src/SimpleTextEditor_07.java
@@ -1,4 +1,4 @@
-///  77 / 100 точки
+
 
 
 import java.util.ArrayDeque;
@@ -10,26 +10,32 @@ public class SimpleTextEditor_07 {
         Scanner scanner = new Scanner(System.in);
         StringBuilder text = new StringBuilder();
         int n = Integer.parseInt(scanner.nextLine());
-        ArrayDeque<String> memorizeText = new ArrayDeque<>();
+        ArrayDeque<String> memorizeTextStack = new ArrayDeque<>();
         for (int i = 0; i <n ; i++) {
          String[] command = scanner.nextLine().split("\\s+");
             switch (command[0]){
                 case "1":
                     text.append(command[1]);
-                    memorizeText.push(text.toString());
+                    memorizeTextStack.push(text.toString());
                     break;
                 case "2":
                     int count = Integer.parseInt(command[1]);
+
                     text = text.delete(text.length()-count,text.length());
-                    memorizeText.push(text.toString());
+                    memorizeTextStack.push(text.toString());
                     break;
                 case "3":
                     int index = Integer.parseInt(command[1]);
                     System.out.println(text.charAt(index-1));
                     break;
                 case "4":
-                    memorizeText.pop();
-                    text = new StringBuilder(memorizeText.peek());
+                    memorizeTextStack.pop();
+                    if (memorizeTextStack.isEmpty()){
+                        text = new StringBuilder();
+                    }else {
+                        text = new StringBuilder(memorizeTextStack.peek());
+                    }
+
 
                     break;
 
